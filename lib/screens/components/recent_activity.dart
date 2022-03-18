dce5d72f14c22d1a9523b2fde9c7ee13a18319ec
Jake Coker <jake.coker91@gmail.com>
2022-03-17 20:01:38 -0700

    Altered Recent Activities

--- lib/screens/components/recent_activity.dart
+++ lib/screens/components/recent_activity.dart
@@ -11,7 +11,11 @@ class RecentActivity extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
         color: widgetColor,
       ),
-      height: 300,
+      // height: 300,
+      constraints: BoxConstraints(
+        minHeight: 300,
+        maxHeight: 600,
+      ),
       width: double.infinity,
       margin: EdgeInsets.fromLTRB(
         appPaddingDouble,
@@ -54,121 +58,120 @@ class _RecentActivityTable extends StatelessWidget {
           Expanded(
             flex: 5,
             child: Container(
+              // height: 300,
               width: double.infinity,
-              child: SingleChildScrollView(
-                child: DataTable(
-                  columns: [
-                    DataColumn(
-                      label: Text(
-                        "Date",
-                        style: TextStyle(
-                          color: whiteColor,
-                        ),
+              child: DataTable(
+                columns: [
+                  DataColumn(
+                    label: Text(
+                      "Date",
+                      style: TextStyle(
+                        color: whiteColor,
                       ),
                     ),
-                    DataColumn(
-                      label: Text(
-                        "User",
-                        style: TextStyle(
-                          color: whiteColor,
-                        ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      "User",
+                      style: TextStyle(
+                        color: whiteColor,
                       ),
                     ),
-                    DataColumn(
-                      label: Text(
-                        "Action",
-                        style: TextStyle(
-                          color: whiteColor,
-                        ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      "Action",
+                      style: TextStyle(
+                        color: whiteColor,
                       ),
                     ),
-                    DataColumn(
-                      label: Text(
-                        "Asset ID",
-                        style: TextStyle(
-                          color: whiteColor,
-                        ),
+                  ),
+                  DataColumn(
+                    label: Text(
+                      "Asset ID",
+                      style: TextStyle(
+                        color: whiteColor,
                       ),
                     ),
-                  ],
-                  dividerThickness: 2.0,
-                  horizontalMargin: appPaddingDouble,
-                  showBottomBorder: true,
-                  rows: [
-                    _DataRowMaker(
-                        DateTime.now().day.toString() +
-                            "-" +
-                            DateTime.now().month.toString() +
-                            "-" +
-                            DateTime.now().year.toString(),
-                        "Guest",
-                        "Update",
-                        "11344"),
-                    _DataRowMaker(
-                        DateTime.now().day.toString() +
-                            "-" +
-                            DateTime.now().month.toString() +
-                            "-" +
-                            DateTime.now().year.toString(),
-                        "Guest",
-                        "Update",
-                        "24288"),
-                    _DataRowMaker(
-                        DateTime.now().day.toString() +
-                            "-" +
-                            DateTime.now().month.toString() +
-                            "-" +
-                            DateTime.now().year.toString(),
-                        "Guest",
-                        "Update",
-                        "55543"),
-                    _DataRowMaker(
-                        DateTime.now().day.toString() +
-                            "-" +
-                            DateTime.now().month.toString() +
-                            "-" +
-                            DateTime.now().year.toString(),
-                        "Guest",
-                        "Update",
-                        "42344"),
-                    _DataRowMaker(
-                        DateTime.now().day.toString() +
-                            "-" +
-                            DateTime.now().month.toString() +
-                            "-" +
-                            DateTime.now().year.toString(),
-                        "Guest",
-                        "Update",
-                        "54567"),
-                    _DataRowMaker(
-                        DateTime.now().day.toString() +
-                            "-" +
-                            DateTime.now().month.toString() +
-                            "-" +
-                            DateTime.now().year.toString(),
-                        "Guest",
-                        "Update",
-                        "88755"),
-                    _DataRowMaker(
-                        DateTime.now().day.toString() +
-                            "-" +
-                            DateTime.now().month.toString() +
-                            "-" +
-                            DateTime.now().year.toString(),
-                        "Guest",
-                        "Update",
-                        "67846"),
-                    _DataRowMaker(
-                        DateTime.now().day.toString() +
-                            "-" +
-                            DateTime.now().month.toString() +
-                            "-" +
-                            DateTime.now().year.toString(),
-                        "Guest",
-                        "Update",
-                        "43211"),
-                  ],
-                ),
+                  ),
+                ],
+                dividerThickness: 2.0,
+                horizontalMargin: appPaddingDouble,
+                showBottomBorder: true,
+                rows: [
+                  _DataRowMaker(
+                      DateTime.now().day.toString() +
+                          "-" +
+                          DateTime.now().month.toString() +
+                          "-" +
+                          DateTime.now().year.toString(),
+                      "Guest",
+                      "Update",
+                      "11344"),
+                  _DataRowMaker(
+                      DateTime.now().day.toString() +
+                          "-" +
+                          DateTime.now().month.toString() +
+                          "-" +
+                          DateTime.now().year.toString(),
+                      "Guest",
+                      "Update",
+                      "24288"),
+                  _DataRowMaker(
+                      DateTime.now().day.toString() +
+                          "-" +
+                          DateTime.now().month.toString() +
+                          "-" +
+                          DateTime.now().year.toString(),
+                      "Guest",
+                      "Update",
+                      "55543"),
+                  _DataRowMaker(
+                      DateTime.now().day.toString() +
+                          "-" +
+                          DateTime.now().month.toString() +
+                          "-" +
+                          DateTime.now().year.toString(),
+                      "Guest",
+                      "Update",
+                      "42344"),
+                  _DataRowMaker(
+                      DateTime.now().day.toString() +
+                          "-" +
+                          DateTime.now().month.toString() +
+                          "-" +
+                          DateTime.now().year.toString(),
+                      "Guest",
+                      "Update",
+                      "54567"),
+                  _DataRowMaker(
+                      DateTime.now().day.toString() +
+                          "-" +
+                          DateTime.now().month.toString() +
+                          "-" +
+                          DateTime.now().year.toString(),
+                      "Guest",
+                      "Update",
+                      "88755"),
+                  _DataRowMaker(
+                      DateTime.now().day.toString() +
+                          "-" +
+                          DateTime.now().month.toString() +
+                          "-" +
+                          DateTime.now().year.toString(),
+                      "Guest",
+                      "Update",
+                      "67846"),
+                  _DataRowMaker(
+                      DateTime.now().day.toString() +
+                          "-" +
+                          DateTime.now().month.toString() +
+                          "-" +
+                          DateTime.now().year.toString(),
+                      "Guest",
+                      "Update",
+                      "43211"),
+                ],
               ),
             ),
           ),
