3d521a44b9f134de74297ab5ec0cec7614455ddd
Jake Coker <jake.coker91@gmail.com>
2022-04-12 07:12:30 -0700

    added firebase

--- lib/main.dart
+++ lib/main.dart
@@ -2,27 +2,47 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'screens/dash_board_screen.dart';
 import 'controllers/controller.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:track_it_right/firebase_options.dart';
 
 void main() {
-  runApp(const MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  MyApp({Key? key}) : super(key: key);
+  final Future<FirebaseApp> _fbApp = Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "track-it Right",
-      debugShowCheckedModeBanner: false,
-      home: MultiProvider(
-        providers: [
-          ChangeNotifierProvider(
-            create: (context) => Controller(),
-          ),
-        ],
-        child: DashBoardScreen(),
-      ),
-    );
+        title: "track-it Right",
+        debugShowCheckedModeBanner: false,
+        home: FutureBuilder(
+          future: _fbApp,
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              print('You have an error! ${snapshot.error.toString()}');
+              return Text("Something went wrong...");
+            } else if (snapshot.hasData) {
+              return MultiProvider(
+                providers: [
+                  ChangeNotifierProvider(
+                    create: (context) => Controller(),
+                  ),
+                ],
+                child: DashBoardScreen(),
+              );
+            } else {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+          },
+        ));
   }
 }
